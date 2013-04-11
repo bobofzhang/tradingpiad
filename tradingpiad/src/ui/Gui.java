5b3b8e8703d3e83a90a805f4bf8963e42fd8a355
mrsazegar <mrsazegar@gmail.com>
2013-04-11 14:51:28 +0200

    gui test 2

--- tradingpiad/src/ui/Gui.java
+++ tradingpiad/src/ui/Gui.java
@@ -1,17 +1,85 @@
 package ui;
 
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import market.Currency;
+import market.ExchangeError;
+import market.Market;
+import market.Wallet;
+import market.bitstamp.MarketBitstampVirtual;
+
 
 public class Gui {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExchangeError, NoSuchAlgorithmException, KeyManagementException, IOException {
+		
+		
+		
+		// Pour que ca fonctionne a l'univ !
+		System.setProperty("java.net.useSystemProxies", "true");
+		
+		// Pour r�soudre le probleme des certificats
+		X509TrustManager trm = new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, new TrustManager[] { trm }, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        
+        
+        System.setProperty("http.agent", "");
+		
+		Wallet wal=new Wallet();
+		wal.setAmount(Currency.USD, new BigDecimal(1000.0));
+		wal.setAmount(Currency.BTC, new BigDecimal(10.0));
+		Market m =new MarketBitstampVirtual(Currency.BTC, Currency.USD,wal,60000,System.currentTimeMillis()+600000);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -38,6 +106,52 @@ public class Gui {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[left][206.00,grow]", "[][][][][]"));
+
+		JLabel lblNewLabel = new JLabel("Chose a Market");
+		frame.getContentPane().add(lblNewLabel, "cell 0 0");
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"MtGoc", "BTCE ", "BitStamp"}));
+		frame.getContentPane().add(comboBox, "cell 1 0,growx");
+
+		JLabel lblChoseACurrency = new JLabel("Chose a currency");
+		frame.getContentPane().add(lblChoseACurrency, "cell 0 1");
+
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(Currency.values()));
+		frame.getContentPane().add(comboBox_1, "cell 1 1,growx");
+
+		JLabel lblHowMuchDo = new JLabel("How much do you want to invest ? ");
+		frame.getContentPane().add(lblHowMuchDo, "cell 0 2,alignx trailing");
+
+		textField = new JTextField();
+		frame.getContentPane().add(textField, "cell 1 2,growx");
+		textField.setColumns(10);
+
+		JSeparator separator = new JSeparator();
+		frame.getContentPane().add(separator, "cell 0 4,growx");
+
+		JSeparator separator_1 = new JSeparator();
+		frame.getContentPane().add(separator_1, "flowx,cell 1 4,growx");
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnFile.add(mntmNew);
+
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnFile.add(mntmSave);
+
+		JMenuItem mntmReset = new JMenuItem("Reset");
+		mnFile.add(mntmReset);
+
+		JMenuItem mntmQuit = new JMenuItem("Quit");
+		mnFile.add(mntmQuit);
 	}
 
 }
